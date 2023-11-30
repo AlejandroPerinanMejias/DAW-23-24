7a1445ed3c7557be63599ac8327870d5a2e76875
AlejandroPerinanMejias <alejandro.perinan.mejias.alu@iesfernandoaguilar.es>
2023-11-30 17:26:44 +0100

    jueves

---
+++ DWEC/Tema 3/apuntesExamenJS.js
@@ -0,0 +1,18 @@
+//Para fechas, set, get, 
+    new Date();
+    //.toUTCString()
+
+//Para abrir una ventana
+    window.open("ej14_2.html", "_blank", "width=600,height=400,location=yes");
+    document.write("La ventana secundaria se abrió correctamente");
+
+//Para navegar por el historial
+    history.back();
+    history.forward();
+    window.location.href = indice;
+
+//Nodos (ej12)
+    document.body.nodeName;
+        //.childNodes;
+        //.nodeName;
+        //.nodeType;
