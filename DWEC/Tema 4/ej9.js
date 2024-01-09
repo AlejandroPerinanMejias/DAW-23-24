029a808c86a6ec5327b030d6829a7fd5ef4e0aa5
AlejandroPerinanMejias <alejandro.perinan.mejias.alu@iesfernandoaguilar.es>
2024-01-09 17:26:53 +0100

    go

--- DWEC/Tema 4/ej9.js
+++ DWEC/Tema 4/ej9.js
@@ -1,24 +1,47 @@
-var bdTareas = [];
+let tareas = [];
 
-function anadirTarea() {
-    let nombreTarea = document.getElementById("nombreTarea").value;
-    let descripcionTarea = document.getElementById("descripcionTarea").value;
-    const ID = bdTareas.length+1;
-    let nombre = "Tarea " + ID;
-    let tarea = {
-        ID: ID,
-        nombre: nombreTarea,
-        descripcion: descripcionTarea
+function agregarTarea() {
+    const nombreTarea = document.getElementById("nombreTarea");
+    const descripcionTarea = document.getElementById("descripcionTarea");
+    const tarea = {
+        id: tareas.length + 1,
+        nombre: nombreTarea.value,
+        descripcion: descripcionTarea.value,
+        estado: "Pendiente"
     };
-    bdTareas.push(tarea);
-    console.log(bdTareas);
+    tareas.push(tarea);
+    nombreTarea.value = "";
+    mostrarTareas();
 }
 
-function modificarTarea() {
-
+function completarTarea(id) {
+    for (let i = 0; i < tareas.length; i++) {
+        if (tareas[i].id == id) {
+            tareas[i].estado = "Completada";
+        }
+    }
+    mostrarTareas();
 }
 
-function eliminarTarea() {
-
+function eliminarTarea(id) {
+    tareas = tareas.filter(tarea => tarea.id !== id);
+    mostrarTareas();
 }
 
+function buscarTarea() {
+    let id = document.getElementById("buscarTarea").value;
+    for (let i = 0; i < tareas.length; i++) {
+        if (tareas[i].id == id) {
+            let tarea = tareas[i];
+            document.getElementById("tareaEncontrada").innerHTML = tarea.id + " " + '<strong>' + tarea.nombre + '</strong>' + ": " + tarea.descripcion + ' (' + tarea.estado;
+        }
+    }
+}
+
+function mostrarTareas() {
+    const listaTareas = document.getElementById("listaTareas");
+    listaTareas.innerHTML = "";
+    tareas.forEach(tarea => {
+        listaTareas.innerHTML += '<li>' + tarea.id + " " + '<strong>' + tarea.nombre + '</strong>' + ": " + tarea.descripcion + ' (' + tarea.estado + ') <button onclick="completarTarea(' + tarea.id + ')">Completada</button> <button onclick="eliminarTarea(' + tarea.id + ')">Eliminar tarea</button></li>';
+    });
+}
