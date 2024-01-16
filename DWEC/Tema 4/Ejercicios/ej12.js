007845bce4a607d4b3ef5458534ee39ec0235e17
AlejandroPerinanMejias <alejandro.perinan.mejias.alu@iesfernandoaguilar.es>
2024-01-16 17:27:11 +0100

    sacada js rapida

--- DWEC/Tema 4/Ejercicios/ej12.js
+++ DWEC/Tema 4/Ejercicios/ej12.js
@@ -1,29 +1,49 @@
 let players = [
     peri = {
-        "ID" : 1,
         "name" : "Alejandro Perinan",
+        "initialLevelPoints" : 0,
+        "mediumLevelPoints" : 0,
+        "advancedLevelPoints" : 0,
     }
 ];
 
-function addPlayer() {
+function addNewPlayer() {
     let playerName = document.getElementById("playerName").value;
-    let playerLevel = document.getElementById("level").value;
     let player = {
-        "ID" : players.length + 1,
         "name" : playerName,
-        "initialLevelPoints" : playerLevel
-    }
+        "initialLevelPoints" : 0,
+        "mediumLevelPoints" : 0,
+        "advancedLevelPoints" : 0,
+    };
     players.push(player);
+}
+
+function modifyPlayer() {
+    let playerName = document.getElementById("playerName").value;
+    let points = parseInt(document.getElementById("points").value);
+    let level = document.getElementById("level").value;
+    for (let i = 0; i < players.length; i++) {
+        if (players[i]["name"] == playerName) {
+            players[i][level] += points;
+        };
+    }
 }
 
 function addPoints() {
     let playerName = document.getElementById("playerName").value;
+
     let flag = true;
     for (let i = 0; i < players.length; i++) {
         if (players[i]["name"] == playerName) {
-            
+            flag = false;
         };
     }
-    let points = document.getElementById("points").value;
 
+    if (flag) {
+        addNewPlayer();
+        modifyPlayer();
+    } else modifyPlayer();
+    
+console.log(players);
 }
+
