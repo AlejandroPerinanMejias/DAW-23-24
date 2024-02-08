36cd1a16ad29eb6346ab1d3728290b21a518bd8c
AlejandroPerinanMejias <alejandro.perinan.mejias.alu@iesfernandoaguilar.es>
2024-02-08 19:43:24 +0100

    yau

--- LM/Trimestre 1/Tema 4/relacionFInal/ej4.js
+++ LM/Trimestre 1/Tema 4/relacionFInal/ej4.js
@@ -3,5 +3,15 @@ function check() {
     let edad = document.getElementById("edad").value;
     let interes = document.getElementById("interes").value;
 
-    if () {}
+    if (nombre == "") {
+        alert("Debes rellenar correctamente el campo nombre");
+    } else if (edad < 18) {
+        alert("Debes ser mayor de edad");
+    }
+    else if (interes == "elegir") {
+        alert("Debes seleccionar una opcion en interes");
+    }
+    else {
+        alert("Correcto");
+    }
 }
