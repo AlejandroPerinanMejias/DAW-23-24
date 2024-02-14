2b24c34fb6da3f233644ee1dbbf383189f6b9217
AlejandroPerinanMejias <alejandro.perinan.mejias.alu@iesfernandoaguilar.es>
2024-02-14 16:46:52 +0100

    miercoles

--- LM/Trimestre 1/Tema 4/relacionFInal/ej5.js
+++ LM/Trimestre 1/Tema 4/relacionFInal/ej5.js
@@ -1,3 +1,9 @@
 function check() {
-    
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let correo = document.getElementById("correo").value;
+    let fecha = document.getElementById("fecha").value;
+    let interes = document.getElementById("interes").value;
+    let checkbox = document.getElementById("checkbox").value;
+    let radio = document.getElementsByClassName
 }
